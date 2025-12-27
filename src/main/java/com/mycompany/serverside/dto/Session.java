1e8a50924bcffe1551186a0428ddc2d03fa346ac
Hend Sayed <hendsayed853@gmail.com>
2025-12-27 17:14:52 +0200

    update dto

--- src/main/java/com/mycompany/serverside/dto/Session.java
+++ src/main/java/com/mycompany/serverside/dto/Session.java
@@ -3,6 +3,29 @@ package com.mycompany.serverside.dto;
 import java.security.Timestamp;
 
 public class Session {
-    public int id , player1_id , player2_id , winner_id ;
+
+    private final int id;
+    private final int player1_id;
+    private final int player2_id;
+    private final int winner_id;
     public Timestamp startTime , endTime; 
+
+    public Session(int id, int player1_id, int player2_id, int winner_id, Timestamp startTime, Timestamp endTime) {
+        this.id = id;
+        this.player1_id = player1_id;
+        this.player2_id = player2_id;
+        this.winner_id = winner_id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getWinner_id() {
+        return winner_id;
+    }
+
+    public int getId() { return id; }
+
+    public int getPlayer1_id() { return player1_id;  }
+
+    public int getPlayer2_id() {   return player2_id; }
 }
