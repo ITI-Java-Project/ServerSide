b454382ae82b7b7ab356bf05cf1bb83a669e9c85
Hend Sayed <hendsayed853@gmail.com>
2025-12-27 11:20:25 +0200

    add dto for player and session

---
+++ src/main/java/com/mycompany/serverside/dto/Player.java
@@ -0,0 +1,6 @@
+package com.mycompany.serverside.dto;
+
+public class Player {
+    public int id , score;
+    public String name , email , password , gender;
+}
---
+++ src/main/java/com/mycompany/serverside/dto/Session.java
@@ -0,0 +1,8 @@
+package com.mycompany.serverside.dto;
+
+import java.security.Timestamp;
+
+public class Session {
+    public int id , player1_id , player2_id , winner_id ;
+    public Timestamp startTime , endTime; 
+}
