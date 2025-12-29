ba6bd839cfcf731908b705bf133f0cf8f3a514b0
Hend Sayed <hendsayed853@gmail.com>
2025-12-29 18:42:46 +0200

    add all attributes private in dto classes

--- src/main/java/com/mycompany/serverside/dto/Player.java
+++ src/main/java/com/mycompany/serverside/dto/Player.java
@@ -2,8 +2,8 @@ package com.mycompany.serverside.dto;
 
 public class Player {
     private final int id ;
-    public int score;
-    public String name , email , password , gender;
+    private int score;
+    private String name , email , password , gender;
 
     public Player(int id, int score, String name, String email, String password, String gender) {
         this.id = id;
@@ -12,6 +12,30 @@ public class Player {
         this.email = email;
         this.password = password;
         this.gender = gender;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
     public int getId() {  return id; }
