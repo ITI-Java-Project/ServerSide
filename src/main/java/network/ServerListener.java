27b46ad750f2fb3c52b5240c5cf22dc969846ff8
Muaz osama <moazosama204@gmail.com>
2026-01-07 14:38:49 +0200

    spliting onMessage method

--- src/main/java/network/ServerListener.java
+++ src/main/java/network/ServerListener.java
@@ -23,67 +23,92 @@ public class ServerListener implements MessageListener {
     @Override
     public void onMessage(String msg, ClientHandler client) {
         System.out.println("Received from client: " + msg);
+        String command = msg.split(" ", 2)[0];
 
-        // MOVE row col
-        if (msg.startsWith("MOVE")) {
-            String[] parts = msg.split(" ");
-            if (parts.length >= 3) {
-                try {
-                    int row = Integer.parseInt(parts[1]);
-                    int col = Integer.parseInt(parts[2]);
+        switch (command) {
+            case "MOVE":
+                moveAction(msg, client);
+                break;
 
-                    sessionManager.handleMove(client, row, col);
+            case "GET_AVAILABLE_PLAYERS":
+                getAvailablePlayersAction(msg, client);
+                break;
 
-                } catch (NumberFormatException e) {
-                    client.send("INVALID_MOVE_FORMAT");
-                }
-            } else {
-                client.send("INVALID_MOVE_FORMAT");
-            }
-        } else if (msg.equals("GET_AVAILABLE_PLAYERS")) {
-            String playersData = sessionManager.getAvailablePlayersAsJsonString(client);
-            System.out.println("SessionManager Log : " + playersData);
-            if (playersData.isEmpty()) {
-                client.send("AVAILABLE_PLAYERS:NONE");
-            } else {
-                client.send("AVAILABLE_PLAYERS:" + playersData);
-            }
-        } else if (msg.startsWith("REGISTER")) {
-            String[] parts = msg.split(" ");
-            if (parts.length == 4) {
-                String username = parts[1];
-                String email = parts[2];
-                String password = parts[3];
+            case "REGISTER":
+                registerAction(msg, client);
+                break;
 
-                Player player = PlayerDAO.register(username, email, password);
-                if (player != null) {
-                    client.setPlayer(player);
-                    sessionManager.addPlayer(client);
-                    client.send("REGISTERED");
-                } else {
-                    client.send("REGISTER_FAILED");
-                }
-            } else {
-                client.send("INVALID_REGISTER_FORMAT");
-            }
-        } else if (msg.startsWith("LOGIN")) {
-            String[] parts = msg.split(" ");
-            if (parts.length == 3) {
-                String username = parts[1];
-                String password = parts[2];
+            case "LOGIN":
+                loginAction(msg, client);
+                break;
 
-                Player player = PlayerDAO.login(username, password);
-                if (player != null) {
-                    client.setPlayer(player);
-                    sessionManager.addPlayer(client);
-                    client.send("LOGIN_SUCCESS");
-                } else {
-                    client.send("LOGIN_FAILED : username or password isn't correct");
-                }
-            }
-
-        } else {
-            client.send("INVALID_LOGIN_FORMAT");
+            default:
+                client.send("INVALID_LOGIN_FORMAT");
         }
     }
+
+    private void moveAction(String msg, ClientHandler client) {
+        String[] parts = msg.split(" ");
+        if (parts.length >= 3) {
+            try {
+                int row = Integer.parseInt(parts[1]);
+                int col = Integer.parseInt(parts[2]);
+
+                sessionManager.handleMove(client, row, col);
+
+            } catch (NumberFormatException e) {
+                client.send("INVALID_MOVE_FORMAT");
+            }
+        } else {
+            client.send("INVALID_MOVE_FORMAT");
+        }
+    }
+
+    private void getAvailablePlayersAction(String msg, ClientHandler client) {
+        String playersData = sessionManager.getAvailablePlayersAsJsonString(client);
+        System.out.println("SessionManager Log : " + playersData);
+        if (playersData.isEmpty()) {
+            client.send("AVAILABLE_PLAYERS:NONE");
+        } else {
+            client.send("AVAILABLE_PLAYERS:" + playersData);
+        }
+    }
+
+    private void registerAction(String msg, ClientHandler client) {
+        String[] parts = msg.split(" ");
+        if (parts.length == 4) {
+            String username = parts[1];
+            String email = parts[2];
+            String password = parts[3];
+
+            Player player = PlayerDAO.register(username, email, password);
+            if (player != null) {
+                client.setPlayer(player);
+                sessionManager.addPlayer(client);
+                client.send("REGISTERED");
+            } else {
+                client.send("REGISTER_FAILED");
+            }
+        } else {
+            client.send("INVALID_REGISTER_FORMAT");
+        }
+    }
+
+    private void loginAction(String msg, ClientHandler client) {
+        String[] parts = msg.split(" ");
+        if (parts.length == 3) {
+            String username = parts[1];
+            String password = parts[2];
+
+            Player player = PlayerDAO.login(username, password);
+            if (player != null) {
+                client.setPlayer(player);
+                sessionManager.addPlayer(client);
+                client.send("LOGIN_SUCCESS");
+            } else {
+                client.send("LOGIN_FAILED : username or password isn't correct");
+            }
+        }
+    }
+
 }
