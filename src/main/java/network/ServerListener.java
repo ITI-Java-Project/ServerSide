4c41ef9a89a1cbaa95acb58d984bce3c0ed8c79e
ahmed-sala <ahmeds66210@gmail.com>
2026-01-12 13:18:22 +0200

    feat: make leaderboard feature

--- src/main/java/network/ServerListener.java
+++ src/main/java/network/ServerListener.java
@@ -1,6 +1,8 @@
 package network;
 
+import com.google.gson.Gson;
 import com.mycompany.serverside.dto.*;
+import java.util.List;
 import session.SessionManager;
 
 public class ServerListener implements MessageListener {
@@ -42,7 +44,9 @@ public class ServerListener implements MessageListener {
             case "LOGIN":
                 loginAction(msg, client);
                 break;
-
+            case "GET_ALL_PLAYERS":
+                getAllPlayersAction(client);
+                break;
             case "INVITE":
                 handleInvitationRequest(msg, client);
                 break;
@@ -59,6 +63,21 @@ public class ServerListener implements MessageListener {
             default:
                 client.send("INVALID_LOGIN_FORMAT");
         }
+    }
+
+    private void getAllPlayersAction(ClientHandler client) {
+
+        List<Player> players = PlayerDAO.getAllPlayers();
+
+        if (players == null || players.isEmpty()) {
+            client.send("ALL_PLAYERS:NONE");
+            return;
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(players);
+
+        client.send("ALL_PLAYERS:" + json);
     }
 
     private void moveAction(String msg, ClientHandler client) {
